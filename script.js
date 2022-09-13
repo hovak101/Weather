2b6688cf529f8b821cac9f09c599eaceb88644e6
Alex Hovakimyan <hovak101@gmail.com>
2022-09-12 23:08:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,89 @@
+// maps
+var map;
+var infowindow;
+var query = "Mountain View";
+
 // imperial --> farenheit(default)
 // metric --> celcius
 let units = "imperial";
-let unitsSym = "F";
-
+let unitsSym = "°F";
 const fahren = document.querySelector("#fahrenheitButton");
 const celcius = document.querySelector("#celciusButton");
+
+//search bar 
 const searchBar = document.querySelector("#search");
+const submitButton = document.querySelector("#submit");
+
+//city
 const cityHolder = document.querySelector("#cityName");
+let recentCity = document.querySelector("#recent");
+
+//time
+const timeZoneHolder = document.querySelector("#timeZone");
+
+//weather related
 const weathConHolder = document.querySelector("#weatherCondition");
 const tempHolder = document.querySelector("#temp");
-const submitButton = document.querySelector("#submit");
-const recentCity = document.querySelector("#recent");
+const icon = document.querySelector("#weatherImg");
+const windIcon = document.querySelector("#windImg");
+const windHolder = document.querySelector("#wind");
+const maxAndMinHolder = document.querySelector("#tempMaxAndMin");
+const humidityHolder = document.querySelector("#humidity");
+const pressureHolder = document.querySelector("#pressure");
+const feelsLikeHolder = document.querySelector("#feelsLike");
+
+//forecast
+const weathConHolder1 = document.querySelector("#weatherCondition_c1");
+const tempHolder1 = document.querySelector("#temp_c1");
+const icon1 = document.querySelector("#weatherImg_c1");
+const time1 = document.querySelector("#time_c1");
+
+const weathConHolder2 = document.querySelector("#weatherCondition_c2");
+const tempHolder2 = document.querySelector("#temp_c2");
+const icon2 = document.querySelector("#weatherImg_c2");
+const time2 = document.querySelector("#time_c2");
+
+const weathConHolder3 = document.querySelector("#weatherCondition_c3");
+const tempHolder3 = document.querySelector("#temp_c3");
+const icon3 = document.querySelector("#weatherImg_c3");
+const time3 = document.querySelector("#time_c3");
+
+const weathConHolder4 = document.querySelector("#weatherCondition_c4");
+const tempHolder4 = document.querySelector("#temp_c4");
+const icon4 = document.querySelector("#weatherImg_c4");
+const time4 = document.querySelector("#time_c4");
+
+const weathConHolder5 = document.querySelector("#weatherCondition_c5");
+const tempHolder5 = document.querySelector("#temp_c5");
+const icon5 = document.querySelector("#weatherImg_c5");
+const time5 = document.querySelector("#time_c5");
+
+//STORED INTO ONE OBJECT
+const forecast = {
+  time: [time1, time2, time3, time4, time5],
+  icon: [icon1, icon2, icon3, icon4, icon5],
+  weathConHolder: [weathConHolder1, weathConHolder2, weathConHolder3, weathConHolder4, weathConHolder5],
+  tempHolder: [tempHolder1, tempHolder2, tempHolder3, tempHolder4, tempHolder5],
+}
+
 
 // URL for getting weather info
 let weatherURL;
 
+// URL for getting timezone info
+let timeZoneURL;
+
 // URL for getting location info
 let locURL;
+
+// URL for getting country info
+let countryURL;
+
+let input;
 let city;
 let weatherCondition;
+let lan;
+let lon;
 
 
 
@@ -27,7 +92,30 @@ const weatherHost = "https://api.openweathermap.org/data/2.5/weather";
 const locHost = "https://api.openweathermap.org/geo/1.0/direct";
 const limit = 5;
 const key = "c9975c44f7ec4154c146d4b1f6ab38f8";
+const APIkey = "90cb3f88e7ce8b6b5c6847d4ff7a4195";
+const timeZoneHost = "https://api.ipgeolocation.io/timezone?apiKey=";
+const keyTimeZone = "0d7c87861f254d66ab92fefc2b818471";
 
+// Initialize and add the map
+function initMap() {
+  infowindow = new google.maps.InfoWindow();
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 11,
+  });
+  var request = {
+    query: query,
+    fields: ['name', 'geometry'],
+  };
+  var service = new google.maps.places.PlacesService(map);
+  service.findPlaceFromQuery(request, function(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        console.log("Map Signal");
+      }
+      map.setCenter(results[0].geometry.location);
+    }
+  });
+}
 
 // input: city name
 // output: object containing latitude and longitude
@@ -35,13 +123,75 @@ async function cityToCoord(city) {
   locURL = `${locHost}?q=${city}&limit=${limit}&appid=${key}`;
   const json = await fetch(locURL);
   const data = await json.json();
-  var coord = {  
+  console.log(data);
+  var coord = {
+    city: data[0].name,
+    country_code: data[0].country,
     lat: data[0].lat,
     lon: data[0].lon
+
   };
-  
-  return coord
+
+  return coord;
 }
+
+function removeElement(element, array) {
+  const index = array.indexOf(element);
+
+  if (index > -1) {
+    array.splice(index, 1);
+  }
+}
+
+// favorite
+// The line below will delete all saved favorites
+// localStorage.setItem("favorited", '');
+
+let favoriteLocations = [];
+const favoriteButton = document.querySelector("#favoriteButton");
+recentCity.innerHTML = localStorage.getItem("favorited");
+
+function restoreFavorites() {
+  for (let i = 1; i <= recentCity.childElementCount; i++) {
+    const container = document.querySelector(`#recent div:nth-child(${i})`);
+    const actionButton = container.querySelector("button:nth-child(1)");
+    const deleteButton = container.querySelector("button:nth-child(2)");
+    actionButton.addEventListener("click", function() {
+      input = actionButton.innerHTML;
+      weatherHandler();
+      query = input;
+      initMap();
+    });
+
+    actionButton.addEventListener("mouseover", function() {
+      deleteButton.classList.remove("hidden");
+    });
+
+    deleteButton.addEventListener("click", function() {
+      removeElement(actionButton.innerHTML, favoriteLocations);
+      container.remove();
+      localStorage.setItem("favorited", recentCity.innerHTML);
+    });
+
+    container.addEventListener("mouseleave", function() {
+      deleteButton.classList.add("hidden");
+    });
+
+    favoriteLocations.push(actionButton.innerHTML);
+  }
+}
+
+restoreFavorites();
+
+
+async function getCountryInfo(code) {
+  countryURL = `https://restcountries.com/v2/alpha?codes=${code}`;
+  const json = await fetch(countryURL);
+  const data = await json.json();
+
+  return data;
+}
+
 
 // input: latitude, longitude
 // output: weather-info JSON
@@ -49,9 +199,36 @@ async function getCityWeather(lat, lon) {
   weatherURL = `${weatherHost}?lat=${lat}&lon=${lon}&units=${units}&appid=${key}`;
   const json = await fetch(weatherURL);
   const data = await json.json();
-  
+
   return data;
 }
+
+//weather forecasting
+
+async function getForecast(lat, lon) {
+  let newForecast = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=40&appid=${APIkey}&units=${units}`);
+  const data = await newForecast.json();
+  return data;
+}
+
+
+// input: latitude, longitude
+// output:12 hour time at position
+async function coordToTimeZone(lat, lon) {
+  timeZoneURL = `${timeZoneHost}${keyTimeZone}&lat=${lat}&long=${lon}`;
+   const jsonTest = await fetch(timeZoneURL).then(function(response){
+     if(!response.ok){
+        timeZoneHolder.classList.add("hidden");
+        return;
+     }
+   }); 
+  const json = await fetch(timeZoneURL);
+  const data = await json.json();
+  const time = data.time_12;
+  return time;
+}
+
+// GETTING WEATHER DATA
 
 function getWeatherDesc(data) {
   return data.weather[0].description;
@@ -61,8 +238,78 @@ function getTemp(data) {
   return data.main.temp;
 }
 
-function getCityName(data) {
-  return data.name;
+function getCityName(coord) {
+  return coord.city;
+}
+
+function getWeatherIconCode(data) {
+  return data.weather[0].icon;
+}
+
+// GETTING FORECAST DATA
+
+function getForecastTime(data, i) {
+  return data.list[i].dt_txt.slice(5, data.list[i].dt_txt.slice.length - 11);
+}
+
+function getForecastDesc(data, i) {
+  return data.list[i].weather[0].description;
+}
+
+function getForecastTemp(data, i) {
+  return data.list[i].main.temp;
+}
+
+function getForecastIcon(data, i) {
+  return data.list[i].weather[0].icon;
+}
+
+// wind info 
+
+function getWindInfo(data) {
+  const windSpeed = data.wind.speed;
+  let windDirectionDegree = parseInt(data.wind.deg);
+  let windDirection = "";
+  // windDirectionDegree will always be from 0<=x<=360
+  if (windDirectionDegree == 0) {
+    windDirection = "N";
+  } else if (windDirectionDegree < 90) {
+    windDirection = "NE";
+  } else if (windDirectionDegree == 90) {
+    windDirection = "E";
+  } else if (windDirectionDegree < 180) {
+    windDirection = "SE";
+  } else if (windDirectionDegree == 180) {
+    windDirection = "S";
+  } else if (windDirectionDegree < 270) {
+    windDirection = "SW";
+  } else if (windDirectionDegree == 270) {
+    windDirection = "W";
+  } else if (windDirectionDegree <= 360) {
+    windDirection = "NW";
+  }
+  windIcon.setAttribute("src", "icons/" + windDirection + ".png");
+  windIcon.classList.remove("hidden");
+  return "Wind: " + windSpeed + "mph " + windDirection;
+}
+
+function getWeatherHumidity(data) {
+  return "Humidity: " + parseInt(data.main.humidity) + "%";
+}
+
+function getPressure(data) {
+  return "Pressure: " + data.main.pressure + " hPa";
+}
+
+function getFeelsLike(data) {
+  return parseInt(data.main.feels_like);
+}
+
+//TIME
+
+function updateTime() {
+  updateTimeZoneInfo();
+  setInterval(updateTimeZoneInfo, 60000);
 }
 
 // Handles all asynchrounous API related stuff
@@ -70,52 +317,143 @@ function getCityName(data) {
 // Side note: is there a better way to do this?
 // Should I be using promises or callbacks and stuff? 
 async function weatherHandler() {
-  let coord;
-  let lat;
-  let lon;
-  let data;
-
-  coord = await cityToCoord(city);
+  const coord = await cityToCoord(input);
   lat = coord.lat;
   lon = coord.lon;
-  
-  data = await getCityWeather(lat, lon);
-  
-  cityHolder.innerHTML = getCityName(data);
-  weathConHolder.innerHTML = getWeatherDesc(data);
-  tempHolder.innerHTML = getTemp(data) + unitsSym;
+  city = coord.city;
+
+  updateWeather();
+  updateForecast();
+  updateCountryInfo(coord);
+  updateTime();
+  updateTemp();
 }
+
+async function updateCountryInfo(coord) {
+  const cData = await getCountryInfo(coord.country_code);
+  console.log(cData);
+
+  cityHolder.innerHTML = city + ", " + cData[0].name;
+}
+
+async function updateWeather() {
+  const wData = await getCityWeather(lat, lon);
+  icon.setAttribute("src", "icons/" + getWeatherIconCode(wData) + ".png");
+  weathConHolder.innerHTML = getWeatherDesc(wData);
+  tempHolder.innerHTML = getTemp(wData) + unitsSym;
+  windHolder.innerHTML = getWindInfo(wData);
+  humidityHolder.innerHTML = getWeatherHumidity(wData);
+  pressureHolder.innerHTML = getPressure(wData);
+}
+
+async function updateForecast() {
+  const fData = await getForecast(lat, lon);
+
+  for (let i = 1; i < 6; i++) {
+    forecast.time[i - 1].innerHTML = getForecastTime(fData, -8 + 8 * i);
+    forecast.icon[i - 1].setAttribute("src", "icons/" + getForecastIcon(fData, -8 + 8 * i) + ".png");
+    forecast.weathConHolder[i - 1].innerHTML = getForecastDesc(fData, -8 + 8 * i);
+    forecast.tempHolder[i - 1].innerHTML = getForecastTemp(fData, -8 + 8 * i) + unitsSym;
+  }
+
+  console.log("Forecast Updated!");
+}
+
+async function updateTemp() {
+  const wData = await getCityWeather(lat, lon);
+  const fData = await getForecast(lat, lon);
+
+  tempHolder.innerHTML = getTemp(wData) + unitsSym;
+  maxAndMinHolder.innerHTML = "H: " + wData.main.temp_max + unitsSym + " | L: " + wData.main.temp_min + unitsSym;
+  feelsLikeHolder.innerHTML = "Feels like: " + getFeelsLike(wData) + unitsSym;
+  for (let i = 1; i < 6; i++) {
+    forecast.tempHolder[i - 1].innerHTML = getForecastTemp(fData, -8 + 8 * i) + unitsSym;
+  }
+  console.log("Temperature Updated!")
+}
+
+async function updateTimeZoneInfo() {
+  timeZoneHolder.innerHTML = await coordToTimeZone(lat, lon);
+}
+
 
 // When red button is clicked
 fahren.addEventListener("click", function() {
   units = "imperial";
-  unitsSym = "F";
-  weatherHandler();
+  unitsSym = "°F";
+  updateTemp();
 });
 
 // When blue button is clicked
 celcius.addEventListener("click", function() {
   units = "metric";
-  unitsSym = "C";
-  weatherHandler();
+  unitsSym = "°C";
+  updateTemp();
 });
 
-// It works now that I changed it to this.
-// I think submit button is unnecessary.
-// Also, for some reason, the search bar 
-// doesn't work when I open the website in
-// a new tab. 
-// ---------------------------------------
-// UPDATE: found the problem, its style.css
-// more specifically, .top seems to be the problem. 
+//SEARCH LISTENER
+
 searchBar.addEventListener("change", function() {
-  let input = searchBar.value;
-  city = input;
+  input = searchBar.value;
+  searchBar.value = "";
   weatherHandler();
+  query = input;
+  initMap();
 });
-// submitButton.addEventListener("click",function(){
-//   console.log(searchBar);
-//   recentCity.innerHTML += searchBar+"</br>";
-//   city = searchBar;
-//  weatherHandler();
-// });
+
+function clickHandler() {
+  console.log("clicked!");
+}
+
+//FAVORITES
+
+favoriteButton.addEventListener("click", function() {
+  let noDuplicates = true;
+  const length = favoriteLocations.length;
+
+  for (let i = 0; i < length; i++) {
+    if (favoriteLocations[i] === city) {
+      noDuplicates = false;
+    }
+  }
+
+  if (noDuplicates && city != undefined) {
+    favoriteLocations.push(city);
+    const container = document.createElement("div");
+    const actionButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("hidden");
+    container.appendChild(actionButton);
+    container.appendChild(deleteButton);
+    recentCity.appendChild(container);
+    actionButton.innerHTML = city;
+    deleteButton.innerHTML = "X";
+
+    actionButton.addEventListener("click", function() {
+      input = actionButton.innerHTML;
+      searchBar.value = "";
+      weatherHandler();
+      query = input;
+      initMap();
+    });
+
+    actionButton.addEventListener("mouseenter", function() {
+      deleteButton.classList.remove("hidden");
+    })
+
+    deleteButton.addEventListener("click", function() {
+      removeElement(actionButton.innerHTML, favoriteLocations);
+      container.remove();
+      localStorage.setItem("favorited", recentCity.innerHTML);
+    });
+
+    container.addEventListener("mouseleave", function() {
+      console.log("howdy 13");
+      deleteButton.classList.add("hidden");
+    })
+    
+    initMap();
+
+    localStorage.setItem("favorited", recentCity.innerHTML);
+  }
+});
